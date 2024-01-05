8f409e28da484490a460cd2bca50a377ea714e5c
Abu Naim <abunaim@Abus-MacBook-Air-2.local>
2024-01-06 02:44:24 +0600

    doneDOM

--- code_format.js
+++ code_format.js
@@ -1,33 +1,42 @@
+function handleDeposite() {
+  var convertInput = getConvertedValue("deposite-input", "value");
 
-function handleDeposite(){
-    var inputValue = document.getElementById("deposite-input").value;
-    var convertInput = convertToNumber(inputValue);
-    var depositeAmount = document.getElementById("deposite-amount").innerText;
-    var convertAmount = convertToNumber(depositeAmount);
-    var sum = convertAmount + convertInput;
-    document.getElementById("deposite-amount").innerText = sum;
+  var convertAmount = getConvertedValue("deposite-amount", "innerText");
 
-    var totalAmount = document.getElementById("total-amount").innerText;
-    var convertedTotalAmount = convertToNumber(totalAmount);
-    var totalSum = convertedTotalAmount + convertInput;
-    document.getElementById("total-amount").innerText = totalSum;
-    document.getElementById("deposite-input").value = ""
+  var sum = convertAmount + convertInput;
+
+  setInnertext("deposite-amount", sum);
+
+  var convertedTotalAmount = getConvertedValue("total-amount", "innerText");
+
+  setInnertext("total-amount", totalSum);
+  document.getElementById("deposite-input").value = "";
 }
 
-function handleWithdraw(){
-    var withdrawInput = document.getElementById("withdraw-input").value;
-    var convertWithdraw = convertToNumber(withdrawInput);
-    var withdrawAmount = document.getElementById("withdraw-amount").innerText;
-    var convertWithdrawAmount = convertToNumber(withdrawAmount);
-    var sum = convertWithdrawAmount + convertWithdraw;
-    document.getElementById("withdraw-amount").innerText = sum;
-    var x = document.getElementById("total-amount").innerText
-    var totalBalance = convertToNumber(x);
-    var balance = totalBalance - convertWithdraw;
-    document.getElementById("total-amount").innerText = balance;
-    document.getElementById("withdraw-input").value = ""
+function handleWithdraw() {
+  var convertWithdraw = getConvertedValue("withdraw-input", "value");
+
+  var convertWithdrawAmount = getConvertedValue("withdraw-amount", "innerText");
+
+  var sum = convertWithdrawAmount + convertWithdraw;
+  setInnertext("withdraw-amount", sum);
+
+  var totalBalance = getConvertedValue("total-amount", "innerText");
+  var balance = totalBalance - convertWithdraw;
+  setInnertext("total-amount", balance);
+  document.getElementById("withdraw-input").value = "";
 }
 
-function convertToNumber(value){
+function getConvertedValue(ID, element) {
+  if (element == "innerText") {
+    var value = document.getElementById(ID).innerText;
     return parseFloat(value);
+  }
+  else{
+    var value = document.getElementById(ID).value;
+    return parseFloat(value);
+  }
+}
+function setInnertext(ID, value) {
+  document.getElementById(ID).innerText = value;
 }
